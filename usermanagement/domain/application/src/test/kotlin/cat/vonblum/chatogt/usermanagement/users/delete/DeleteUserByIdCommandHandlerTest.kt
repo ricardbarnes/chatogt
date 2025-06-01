20490df2a5d36b264f736ca235d9e352687b253b
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-06-01 20:20:10 +0200

    wip

--- usermanagement/domain/application/src/test/kotlin/cat/vonblum/chatogt/usermanagement/users/delete/DeleteUserByIdCommandHandlerTest.kt
+++ usermanagement/domain/application/src/test/kotlin/cat/vonblum/chatogt/usermanagement/users/delete/DeleteUserByIdCommandHandlerTest.kt
@@ -2,12 +2,10 @@ package cat.vonblum.chatogt.usermanagement.users.delete
 
 import cat.vonblum.chatogt.shared.domain.event.EventBus
 import cat.vonblum.chatogt.usermanagement.users.FindingUsers
-import cat.vonblum.chatogt.usermanagement.users.UserId
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
 
 @ExtendWith(MockitoExtension::class)
