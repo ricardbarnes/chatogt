31cbb958ea004042bee3083a6caa71e890575893
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-08-16 18:54:46 +0200

    wip

--- usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/controller/GrpcUserController.kt
+++ usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/controller/GrpcUserController.kt
@@ -1,5 +1,6 @@
 package cat.vonblum.chatogt.usermanagement.api.controller
 
+import com.google.protobuf.Empty
 import net.devh.boot.grpc.server.service.GrpcService
 import user.UserOuterClass
 import user.UserServiceGrpcKt
@@ -7,9 +8,24 @@ import user.UserServiceGrpcKt
 @GrpcService
 class GrpcUserController : UserServiceGrpcKt.UserServiceCoroutineImplBase() {
 
-    override suspend fun createUser(request: UserOuterClass.User): UserOuterClass.User {
+    override suspend fun createUser(request: UserOuterClass.CreateUserRequest): Empty {
         // TODO
-        return UserOuterClass.User.newBuilder().build()
+        return super.createUser(request)
+    }
+
+    override suspend fun findUserById(request: UserOuterClass.FindUserByIdRequest): UserOuterClass.User {
+        // TODO
+        return super.findUserById(request)
+    }
+
+    override suspend fun findUserByName(request: UserOuterClass.FindUserByNameRequest): UserOuterClass.User {
+        // TODO
+        return super.findUserByName(request)
+    }
+
+    override suspend fun deleteUserById(request: UserOuterClass.DeleteUserByIdRequest): Empty {
+        // TODO
+        return super.deleteUserById(request)
     }
 
 }
