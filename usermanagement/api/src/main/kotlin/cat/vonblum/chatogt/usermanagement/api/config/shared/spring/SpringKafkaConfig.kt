fae98d4436569597eb76389b23b22ba9d266cdf2
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-09-14 19:28:53 +0200

    wip

--- usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/config/shared/spring/SpringKafkaConfig.kt
+++ usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/config/shared/spring/SpringKafkaConfig.kt
@@ -1,17 +1,15 @@
 package cat.vonblum.chatogt.usermanagement.api.config.shared.spring
 
-import cat.vonblum.chatogt.shared.infrastructure.bus.MessageEnvelope
 import cat.vonblum.chatogt.shared.infrastructure.bus.command.MessageProducer
 import cat.vonblum.chatogt.shared.infrastructure.bus.command.kafka.KafkaMessageProducer
-import cat.vonblum.chatogt.shared.infrastructure.config.shared.spring.SpringKafkaConfig as SharedKafkaConfig
 import cat.vonblum.chatogt.usermanagement.api.bus.command.kafka.KafkaCommandBus
 import cat.vonblum.chatogt.usermanagement.api.bus.query.kafka.KafkaQueryBus
-import cat.vonblum.chatogt.usermanagement.api.config.shared.spring.SpringBusProps
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.kafka.core.KafkaTemplate
+import cat.vonblum.chatogt.shared.infrastructure.config.shared.spring.SpringKafkaConfig as SharedKafkaConfig
 
 @Configuration
 @Import(SharedKafkaConfig::class)
