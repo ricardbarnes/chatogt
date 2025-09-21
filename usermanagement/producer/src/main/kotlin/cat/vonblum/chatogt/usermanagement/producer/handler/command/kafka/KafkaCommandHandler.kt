9bb8d17ef413276a6053e9920f973728cee63739
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-09-21 15:31:20 +0200

    wip

--- usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/handler/command/kafka/KafkaCommandHandler.kt
+++ usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/handler/command/kafka/KafkaCommandHandler.kt
@@ -1,6 +1,11 @@
 package cat.vonblum.chatogt.usermanagement.producer.handler.command.kafka
 
-class KafkaCommandHandler(private val mapper: KafkaCommandMapper) {
+import cat.vonblum.chatogt.shared.infrastructure.handler.shared.Handler
+
+class KafkaCommandHandler(
+    private val mapper: KafkaCommandMapper,
+    private val handler: Handler
+) {
 
     // TODO
 
