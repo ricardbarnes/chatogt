90818bdfa04a9b76f163000bbf9e216220fc40a2
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-09-21 19:23:34 +0200

    wip

--- usermanagement/domain/application/src/main/kotlin/cat/vonblum/chatogt/usermanagement/users/create/CreateUserCommandHandler.kt
+++ usermanagement/domain/application/src/main/kotlin/cat/vonblum/chatogt/usermanagement/users/create/CreateUserCommandHandler.kt
@@ -1,5 +1,6 @@
 package cat.vonblum.chatogt.usermanagement.users.create
 
+import cat.vonblum.chatogt.shared.domain.command.CommandHandler
 import cat.vonblum.chatogt.shared.domain.event.EventBus
 import cat.vonblum.chatogt.shared.domain.generator.IdGenerator
 import cat.vonblum.chatogt.usermanagement.users.*
@@ -7,7 +8,7 @@ import cat.vonblum.chatogt.usermanagement.users.*
 class CreateUserCommandHandler(
     private val idGenerator: IdGenerator,
     private val eventBus: EventBus
-) {
+) : CommandHandler {
 
     fun handle(command: CreateUserCommand) = User.create(
         UserId(idGenerator.next()),
--- usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/config/shared/spring/SpringCommandHandlerConfig.kt
+++ usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/config/shared/spring/SpringCommandHandlerConfig.kt
@@ -1,23 +1,30 @@
 package cat.vonblum.chatogt.usermanagement.producer.config.shared.spring
 
+import cat.vonblum.chatogt.shared.domain.command.Command
+import cat.vonblum.chatogt.shared.domain.command.CommandHandler
 import cat.vonblum.chatogt.shared.infrastructure.handler.command.CommandDispatcher
+import cat.vonblum.chatogt.usermanagement.users.create.CreateUserCommand
 import cat.vonblum.chatogt.usermanagement.users.create.CreateUserCommandHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import kotlin.reflect.KClass
 
 @Configuration
 class SpringCommandHandlerConfig {
 
     @Bean
-    fun commandHandlers(): List<Any> {
-        return listOf(
-            CreateUserCommandHandler::class.java,
+    fun commandHandlingMap(
+        createUserCommandHandler: CreateUserCommandHandler,
+    ): Map<KClass<out Command>, CommandHandler> {
+        return mapOf(
+            CreateUserCommand::class as KClass<out Command> to createUserCommandHandler as CommandHandler,
         )
     }
 
     @Bean
-    fun commandDispatcher(handlers: List<Any>): CommandDispatcher {
-        return CommandDispatcher(handlers)
+    fun commandDispatcher(commandHandlingMap: Map<KClass<out Command>, CommandHandler>): CommandDispatcher {
+        return CommandDispatcher(commandHandlingMap)
     }
 
 }
+
