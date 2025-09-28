d267d19d6c1b030c6905d24ee6dfd10843231470
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-09-28 23:29:36 +0200

    wip

--- usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/config/shared/spring/SpringKafkaConfig.kt
+++ usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/config/shared/spring/SpringKafkaConfig.kt
@@ -1,13 +1,17 @@
 package cat.vonblum.chatogt.usermanagement.producer.config.shared.spring
 
+import cat.vonblum.chatogt.shared.domain.command.Command
+import cat.vonblum.chatogt.shared.domain.command.CommandHandler
 import cat.vonblum.chatogt.shared.domain.event.EventBus
+import cat.vonblum.chatogt.shared.domain.query.Query
+import cat.vonblum.chatogt.shared.domain.query.QueryHandler
 import cat.vonblum.chatogt.shared.infrastructure.message.MessageResolver
-import cat.vonblum.chatogt.usermanagement.producer.handler.shared.spring.SpringKafkaSubscriber
 import cat.vonblum.chatogt.usermanagement.producer.bus.event.kafka.KafkaEventBus
-import cat.vonblum.chatogt.usermanagement.producer.handler.command.users.kafka.KafkaUserCommandHandler
-import cat.vonblum.chatogt.usermanagement.producer.handler.query.users.kafka.KafkaUserQueryHandler
+import cat.vonblum.chatogt.usermanagement.producer.handler.shared.kafka.KafkaMessageMapper
+import cat.vonblum.chatogt.usermanagement.producer.handler.shared.kafka.KafkaMessageResolver
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import kotlin.reflect.KClass
 
 @Configuration
 class SpringKafkaConfig {
@@ -18,16 +22,21 @@ class SpringKafkaConfig {
     }
 
     @Bean
-    fun kafkaMessageResolver(
-        kafkaUserCommandHandler: KafkaUserCommandHandler,
-        kafkaUserQueryHandler: KafkaUserQueryHandler,
-    ): MessageResolver {
-        TODO()
+    fun kafkaMessageMapper(): KafkaMessageMapper {
+        return KafkaMessageMapper()
     }
 
     @Bean
-    fun springKafkaSubscriber(messageResolver: MessageResolver): SpringKafkaSubscriber {
-        return SpringKafkaSubscriber(messageResolver)
+    fun kafkaMessageResolver(
+        commandHandlers: Map<KClass<out Command>, CommandHandler>,
+        queryHandlers: Map<KClass<out Query>, QueryHandler>,
+        mapper: KafkaMessageMapper
+    ): MessageResolver {
+        return KafkaMessageResolver(
+            commandHandlers,
+            queryHandlers,
+            mapper
+        )
     }
 
 }
--- usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/handler/shared/kafka/KafkaMessageResolver.kt
+++ usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/handler/shared/kafka/KafkaMessageResolver.kt
@@ -1,13 +1,16 @@
 package cat.vonblum.chatogt.usermanagement.producer.handler.shared.kafka
 
+import cat.vonblum.chatogt.shared.domain.command.Command
 import cat.vonblum.chatogt.shared.domain.command.CommandHandler
+import cat.vonblum.chatogt.shared.domain.query.Query
 import cat.vonblum.chatogt.shared.domain.query.QueryHandler
 import cat.vonblum.chatogt.shared.infrastructure.message.Message
 import cat.vonblum.chatogt.shared.infrastructure.message.MessageResolver
+import kotlin.reflect.KClass
 
 class KafkaMessageResolver(
-    commandHandlers: List<CommandHandler>,
-    queryHandlers: List<QueryHandler>,
+    commandHandlers: Map<KClass<out Command>, CommandHandler>,
+    queryHandlers: Map<KClass<out Query>, QueryHandler>,
     mapper: KafkaMessageMapper
 ) : MessageResolver {
 
