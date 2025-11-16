dcbb8c3631db580b554fe42e854c3d8e904fe9c8
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-11-16 17:12:28 +0100

    wip

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,15 @@
 rootProject.name = "chatogt"
 
 include(
-    // Shared libraries
     ":shared:domain",
     ":shared:infrastructure",
 
-    // User Management Bounded Context
     ":usermanagement:domain:core",
     ":usermanagement:domain:application",
     ":usermanagement:api",
     ":usermanagement:producer",
     ":usermanagement:consumer",
 
-    // Chat Bounded Context
     ":chat:domain:core",
     ":chat:domain:application",
     ":chat:api",
