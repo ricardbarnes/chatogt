d59ff624c5dd6b73a2e4d6a42be9d63691fdfaed
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-11-16 15:57:14 +0100

    wip

--- usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/config/shared/spring/SpringGrpcConfig.kt
+++ usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/config/shared/spring/SpringGrpcConfig.kt
@@ -4,5 +4,9 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ComponentScan("cat.vonblum.chatogt.usermanagement.api.controller.users.grpc")
-class SpringConfig
+@ComponentScan(
+    value = [
+        "cat.vonblum.chatogt.usermanagement.api.controller.users.grpc",
+    ]
+)
+class SpringGrpcConfig
--- usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/entrypoint/spring/SpringEntrypoint.kt
+++ usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/entrypoint/spring/SpringEntrypoint.kt
@@ -5,7 +5,11 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan("cat.vonblum.chatogt.usermanagement.api.config.shared.spring")
+@ComponentScan(
+    value = [
+        "cat.vonblum.chatogt.usermanagement.api.config.shared.spring",
+    ]
+)
 class SpringEntrypoint
 
 fun main(args: Array<String>) {
