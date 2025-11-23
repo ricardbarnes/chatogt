2c654001f773457caffc5748f0193a58631dfe3b
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-11-23 23:08:18 +0100

    wip

--- usermanagement/api/build.gradle.kts
+++ usermanagement/api/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "cat.vonblum"
