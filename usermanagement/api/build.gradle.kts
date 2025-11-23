4f0b5293e9f21f7d4087b397578aea807139001c
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-11-23 22:44:36 +0100

    wip

--- usermanagement/api/build.gradle.kts
+++ usermanagement/api/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.google.protobuf.gradle.id
-
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
@@ -18,6 +16,7 @@ repositories {
 dependencies {
     implementation(project(":shared:domain"))
     implementation(project(":shared:infrastructure"))
+    implementation(project(":usermanagement:shared"))
     implementation(project(":usermanagement:domain:core"))
     implementation(project(":usermanagement:domain:application"))
     implementation("org.springframework.boot:spring-boot-starter:3.4.4")
@@ -25,52 +24,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.4.4")
-    implementation("io.grpc:grpc-kotlin-stub:1.4.3")
-    implementation("com.google.protobuf:protobuf-java:4.28.2")
     implementation("net.devh:grpc-server-spring-boot-starter:2.15.0.RELEASE")
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.4")
-}
-
-tasks.processResources {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE // TODO: investigate why this is needed
-
-    from(sourceSets.main.get().resources.srcDirs) {
-        exclude("**/*.proto")
-    }
-}
-
-protobuf {
-    protoc {
-        artifact = "com.google.protobuf:protoc:3.25.3"
-    }
-
-    plugins {
-        id("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.72.0"
-        }
-        id("grpckt") {
-            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.3:jdk8@jar"
-        }
-    }
-
-    generateProtoTasks {
-        all().forEach {
-            it.plugins {
-                id("grpc")
-                id("grpckt")
-            }
-        }
-    }
-}
-
-sourceSets {
-    main {
-        proto {
-            srcDir("src/main/proto")
-        }
-        resources {
-            exclude("**/*.proto")
-        }
-    }
-
 }
--- usermanagement/shared/build.gradle.kts
+++ usermanagement/shared/build.gradle.kts
@@ -1,7 +1,10 @@
+import com.google.protobuf.gradle.id
+
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "cat.vonblum"
@@ -16,5 +19,30 @@ tasks.test {
 }
 
 dependencies {
-    // add shared dependencies here
+    api("io.grpc:grpc-kotlin-stub:1.4.3")
+    api("com.google.protobuf:protobuf-java:4.28.2")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.3"
+    }
+
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.72.0"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.3:jdk8@jar"
+        }
+    }
+
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+        }
+    }
 }
