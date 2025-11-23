203a12e0f6474ef81c868d78b94faf943d0fb9d1
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-11-23 22:18:43 +0100

    wip

--- usermanagement/api/build.gradle.kts
+++ usermanagement/api/build.gradle.kts
@@ -20,24 +20,30 @@ dependencies {
     implementation(project(":shared:infrastructure"))
     implementation(project(":usermanagement:domain:core"))
     implementation(project(":usermanagement:domain:application"))
-    implementation(project(":usermanagement:shared"))
     implementation("org.springframework.boot:spring-boot-starter:3.4.4")
     implementation("org.springframework.kafka:spring-kafka:3.3.4")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.4.4")
-    implementation("net.devh:grpc-server-spring-boot-starter:2.15.0.RELEASE")
-    implementation("com.google.protobuf:protobuf-java:4.28.2")
     implementation("io.grpc:grpc-kotlin-stub:1.4.3")
-    implementation("io.grpc:grpc-stub:1.64.0")
-    implementation("io.grpc:grpc-protobuf:1.64.0")
+    implementation("com.google.protobuf:protobuf-java:4.28.2")
+    implementation("net.devh:grpc-server-spring-boot-starter:2.15.0.RELEASE")
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.4")
+}
+
+tasks.processResources {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE // TODO: investigate why this is needed
+
+    from(sourceSets.main.get().resources.srcDirs) {
+        exclude("**/*.proto")
+    }
 }
 
 protobuf {
     protoc {
         artifact = "com.google.protobuf:protoc:3.25.3"
     }
+
     plugins {
         id("grpc") {
             artifact = "io.grpc:protoc-gen-grpc-java:1.72.0"
@@ -46,6 +52,7 @@ protobuf {
             artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.3:jdk8@jar"
         }
     }
+
     generateProtoTasks {
         all().forEach {
             it.plugins {
@@ -54,4 +61,16 @@ protobuf {
             }
         }
     }
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("src/main/proto")
+        }
+        resources {
+            exclude("**/*.proto")
+        }
+    }
+
 }
