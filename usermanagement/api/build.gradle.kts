0adc983d93df3c7a762573600a8edd071f01d582
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-11-24 22:21:50 +0100

    wip

--- usermanagement/api/build.gradle.kts
+++ usermanagement/api/build.gradle.kts
@@ -1,8 +1,11 @@
+import com.google.protobuf.gradle.id
+
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "cat.vonblum"
@@ -24,5 +27,33 @@ dependencies {
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.4.4")
     implementation("net.devh:grpc-server-spring-boot-starter:2.15.0.RELEASE")
+    implementation("com.google.protobuf:protobuf-java:4.28.2")
+    implementation("io.grpc:grpc-kotlin-stub:1.4.3")
+    implementation("io.grpc:grpc-stub:1.64.0")
+    implementation("io.grpc:grpc-protobuf:1.64.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.4")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.3"
+    }
+
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.72.0"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.3:jdk8@jar"
+        }
+    }
+
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+        }
+    }
 }
--- usermanagement/shared/build.gradle.kts
+++ usermanagement/shared/build.gradle.kts
@@ -1,10 +1,7 @@
-import com.google.protobuf.gradle.id
-
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "cat.vonblum"
@@ -16,35 +13,4 @@ repositories {
 
 tasks.test {
     useJUnitPlatform()
-}
-
-dependencies {
-    api("com.google.protobuf:protobuf-java:4.28.2")
-    api("io.grpc:grpc-kotlin-stub:1.4.3")
-    api("io.grpc:grpc-stub:1.64.0")
-    api("io.grpc:grpc-protobuf:1.64.0")
-}
-
-protobuf {
-    protoc {
-        artifact = "com.google.protobuf:protoc:3.25.3"
-    }
-
-    plugins {
-        id("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.72.0"
-        }
-        id("grpckt") {
-            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.3:jdk8@jar"
-        }
-    }
-
-    generateProtoTasks {
-        all().forEach {
-            it.plugins {
-                id("grpc")
-                id("grpckt")
-            }
-        }
-    }
 }
