284324dd680bbf02f81482658be58ca34e2951b4
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-11-24 23:54:35 +0100

    wip

--- usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/config/shared/spring/SpringKafkaConfig.kt
+++ usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/config/shared/spring/SpringKafkaConfig.kt
@@ -1,19 +1,16 @@
 package cat.vonblum.chatogt.usermanagement.api.config.shared.spring
 
-import cat.vonblum.chatogt.usermanagement.infrastructure.io.message.MessageProducer
-import cat.vonblum.chatogt.usermanagement.api.bus.command.spring.SpringKafkaMessageProducer
 import cat.vonblum.chatogt.usermanagement.api.bus.command.kafka.KafkaCommandBus
 import cat.vonblum.chatogt.usermanagement.api.bus.command.kafka.KafkaCommandMapper
+import cat.vonblum.chatogt.usermanagement.api.bus.command.spring.SpringKafkaMessageProducer
 import cat.vonblum.chatogt.usermanagement.api.bus.query.kafka.KafkaQueryBus
+import cat.vonblum.chatogt.usermanagement.infrastructure.io.message.MessageProducer
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
 import org.springframework.kafka.core.KafkaTemplate
-import cat.vonblum.chatogt.usermanagement.infrastructure.config.spring.SpringKafkaConfig as SharedKafkaConfig
 
 @Configuration
-@Import(SharedKafkaConfig::class)
 @EnableConfigurationProperties(SpringBusProps::class)
 class SpringKafkaConfig {
 
