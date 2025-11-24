4833865766cd0343147bd50b60a4504ad02bc521
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-11-24 22:58:56 +0100

    wip

--- usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/entrypoint/spring/SpringEntrypoint.kt
+++ usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/entrypoint/spring/SpringEntrypoint.kt
@@ -3,7 +3,11 @@ package cat.vonblum.chatogt.usermanagement.api.entrypoint.spring
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["cat.vonblum.chatogt.usermanagement.api"])
+@SpringBootApplication(
+    scanBasePackages = [
+        "cat.vonblum.chatogt.usermanagement.api"
+    ]
+)
 class SpringEntrypoint
 
 fun main(args: Array<String>) {
