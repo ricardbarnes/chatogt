83ae453093a6a36d49fe5c3fa6951d13f4acff9f
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-11-24 23:50:14 +0100

    wip

--- shared/infrastructure/src/main/kotlin/cat/vonblum/chatogt/usermanagement/infrastructure/config/spring/SpringKafkaConfig.kt
+++ shared/infrastructure/src/main/kotlin/cat/vonblum/chatogt/usermanagement/infrastructure/config/spring/SpringKafkaConfig.kt
@@ -4,15 +4,14 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.BytesDeserializer
+import org.apache.kafka.common.serialization.BytesSerializer
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
-import org.apache.kafka.common.serialization.UUIDDeserializer
-import org.apache.kafka.common.serialization.UUIDSerializer
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.util.*
 
 @Configuration
 class SpringKafkaConfig {
@@ -23,15 +22,15 @@ class SpringKafkaConfig {
     ): Map<String, Any> {
         return mapOf(
             ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
-            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to UUIDSerializer::class.java,
-            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to BytesSerializer::class.java
         )
     }
 
     @Bean
     fun kafkaProducer(
         @Qualifier("kafkaProducerProperties") properties: Map<String, Any>
-    ): KafkaProducer<UUID, String> {
+    ): KafkaProducer<String, ByteArray> {
         return KafkaProducer(properties)
     }
 
@@ -41,15 +40,15 @@ class SpringKafkaConfig {
     ): Map<String, Any> {
         return mapOf(
             ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
-            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to UUIDDeserializer::class.java,
-            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to BytesDeserializer::class.java
         )
     }
 
     @Bean
     fun kafkaConsumer(
         @Qualifier("kafkaConsumerProperties") properties: Map<String, Any>
-    ): KafkaConsumer<UUID, String> {
+    ): KafkaConsumer<String, ByteArray> {
         return KafkaConsumer(properties)
     }
 
