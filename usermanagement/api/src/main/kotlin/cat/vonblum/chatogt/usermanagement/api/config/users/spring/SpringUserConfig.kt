6f1f5b192dd5093b688e86731e99afae54cd43e4
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-11-24 23:25:47 +0100

    wip

--- usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/config/users/spring/SpringUserConfig.kt
+++ usermanagement/api/src/main/kotlin/cat/vonblum/chatogt/usermanagement/api/config/users/spring/SpringUserConfig.kt
@@ -21,7 +21,11 @@ class SpringUserConfig {
         commandBus: CommandBus,
         queryBus: QueryBus
     ): GrpcUserController {
-        return GrpcUserController(mapper, commandBus, queryBus)
+        return GrpcUserController(
+            mapper,
+            commandBus,
+            queryBus
+        )
     }
 
 }
