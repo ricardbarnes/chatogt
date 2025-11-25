2c61d6f79602861c5e21a07cd9273d122783c06a
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-11-25 22:09:05 +0100

    wip

--- usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/entrypoint/spring/SpringEntrypoint.kt
+++ usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/entrypoint/spring/SpringEntrypoint.kt
@@ -2,14 +2,13 @@ package cat.vonblum.chatogt.usermanagement.producer.entrypoint.spring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
-@ComponentScan(
-    basePackages = [
+@SpringBootApplication(
+    scanBasePackages = [
         "cat.vonblum.chatogt.usermanagement.producer.config.shared.spring",
+        "cat.vonblum.chatogt.usermanagement.producer.config.users.spring",
     ]
 )
-@SpringBootApplication
 class SpringEntrypoint
 
 fun main(args: Array<String>) {
