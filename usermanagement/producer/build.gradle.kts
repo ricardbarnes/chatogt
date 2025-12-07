838a19c699eac0999ad56efae0906b2756d53d3f
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-12-07 20:07:39 +0100

    wip

--- usermanagement/producer/build.gradle.kts
+++ usermanagement/producer/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-//    id("org.springframework.boot") version "3.4.5"
-//    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "cat.vonblum"
@@ -27,6 +25,5 @@ dependencies {
     implementation("org.springframework.kafka:spring-kafka:3.3.4")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.4.4")
-
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.4")
 }
