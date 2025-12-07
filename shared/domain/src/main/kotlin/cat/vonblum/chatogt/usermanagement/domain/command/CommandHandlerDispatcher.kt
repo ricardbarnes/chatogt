f238262994b74d37d13835765063fcc20f8dc7a5
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-12-07 18:19:25 +0100

    wip

--- shared/domain/src/main/kotlin/cat/vonblum/chatogt/usermanagement/domain/command/CommandHandlerDispatcher.kt
+++ shared/domain/src/main/kotlin/cat/vonblum/chatogt/usermanagement/domain/command/CommandHandlerDispatcher.kt
@@ -7,12 +7,18 @@ class CommandHandlerDispatcher(
     private val handlers: Map<KClass<out Command>, CommandHandler>
 ) {
 
+    companion object {
+
+        const val HANDLER_METHOD_NAME = "handle"
+
+    }
+
     fun dispatch(command: Command) {
         val handler = handlers[command::class]
             ?: throw CommandHandlerNotFound.becauseOf(command::class)
 
         val handleMethod = handler::class.declaredFunctions
-            .firstOrNull { it.name == "handle" }
+            .firstOrNull { it.name == HANDLER_METHOD_NAME }
             ?: throw CommandHandlerMethodNotFound.becauseOf(command::class)
 
         handleMethod.call(handler, command)
