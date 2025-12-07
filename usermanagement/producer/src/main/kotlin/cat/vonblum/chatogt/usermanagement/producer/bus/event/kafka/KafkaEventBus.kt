aa251843db1c8d5592725c7b447a5f6c4d3606a5
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-12-08 00:13:36 +0100

    wip

--- usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/bus/event/kafka/KafkaEventBus.kt
+++ usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/bus/event/kafka/KafkaEventBus.kt
@@ -3,9 +3,14 @@ package cat.vonblum.chatogt.usermanagement.producer.bus.event.kafka
 import cat.vonblum.chatogt.usermanagement.domain.event.Event
 import cat.vonblum.chatogt.usermanagement.domain.event.EventBus
 import cat.vonblum.chatogt.usermanagement.domain.event.UnsupportedEvent
+import cat.vonblum.chatogt.usermanagement.infrastructure.bus.shared.kafka.KafkaHeader
 import cat.vonblum.chatogt.usermanagement.shared.config.spring.SpringBusProps
 import cat.vonblum.chatogt.usermanagement.users.UserCreatedEvent
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.internals.RecordHeader
 import org.springframework.kafka.core.KafkaTemplate
+import user.User
+import java.util.*
 
 class KafkaEventBus(
     private val template: KafkaTemplate<String, ByteArray>,
@@ -24,6 +29,20 @@ class KafkaEventBus(
     private fun dispatchUserCreatedEvent(event: UserCreatedEvent) {
         val topic = props.events["users"]?.kafka?.topic ?: throw UnsupportedEvent.becauseOf(event)
         val payload = mapper.toInfra(event)
+        val record = ProducerRecord(
+            topic,
+            UUID.randomUUID().toString(),
+            payload,
+        ).apply {
+            headers().add(
+                RecordHeader(
+                    KafkaHeader.PROTO_TYPE,
+                    User.UserCreatedEvent::class.java.simpleName.toByteArray(),
+                )
+            )
+        }
+
+        template.send(record)
     }
 
 }
