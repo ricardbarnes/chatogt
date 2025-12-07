1c63323c121350e4d7efb292bce0d6f0f271b1a7
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-12-07 14:09:14 +0100

    wip

--- usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/config/users/spring/SpringUserCommandConfig.kt
+++ usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/config/users/spring/SpringUserCommandConfig.kt
@@ -1,10 +1,16 @@
 package cat.vonblum.chatogt.usermanagement.producer.config.users.spring
 
+import cat.vonblum.chatogt.usermanagement.domain.command.Command
+import cat.vonblum.chatogt.usermanagement.domain.command.CommandHandler
 import cat.vonblum.chatogt.usermanagement.domain.event.EventBus
 import cat.vonblum.chatogt.usermanagement.domain.generator.IdGenerator
+import cat.vonblum.chatogt.usermanagement.producer.handler.command.users.kafka.KafkaUserCommandHandler
+import cat.vonblum.chatogt.usermanagement.producer.handler.command.users.kafka.KafkaUserCommandMapper
+import cat.vonblum.chatogt.usermanagement.users.create.CreateUserCommand
 import cat.vonblum.chatogt.usermanagement.users.create.CreateUserCommandHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import kotlin.reflect.KClass
 
 @Configuration
 class SpringUserCommandConfig {
@@ -17,6 +23,31 @@ class SpringUserCommandConfig {
         return CreateUserCommandHandler(
             idGenerator,
             eventBus
+        )
+    }
+
+    @Bean
+    fun kafkaUserCommandMapper(): KafkaUserCommandMapper {
+        return KafkaUserCommandMapper()
+    }
+
+    @Bean
+    fun userHandlerMap(
+        createUserCommandHandler: CreateUserCommandHandler
+    ): Map<KClass<out Command>, CommandHandler> {
+        return mapOf(
+            CreateUserCommand::class as KClass<out Command> to createUserCommandHandler as CommandHandler,
+        )
+    }
+
+    @Bean
+    fun kafkaUserCommandHandler(
+        userHandlerMap: Map<KClass<out Command>, CommandHandler>,
+        mapper: KafkaUserCommandMapper,
+    ): KafkaUserCommandHandler {
+        return KafkaUserCommandHandler(
+            userHandlerMap,
+            mapper
         )
     }
 
