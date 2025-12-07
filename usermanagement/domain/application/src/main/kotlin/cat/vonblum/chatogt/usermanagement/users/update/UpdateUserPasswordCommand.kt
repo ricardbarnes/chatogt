7d987693f1963c9f0539864cc96ad4fc8e958537
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-12-07 14:18:22 +0100

    wip

--- usermanagement/domain/application/src/main/kotlin/cat/vonblum/chatogt/usermanagement/users/update/UpdateUserPasswordCommand.kt
+++ usermanagement/domain/application/src/main/kotlin/cat/vonblum/chatogt/usermanagement/users/update/UpdateUserPasswordCommand.kt
@@ -3,7 +3,7 @@ package cat.vonblum.chatogt.usermanagement.users.update
 import cat.vonblum.chatogt.usermanagement.domain.command.Command
 import java.util.UUID
 
-class UpdateUserPasswordCommand(
+data class UpdateUserPasswordCommand(
     val id: UUID,
     val password: String,
 ) : Command
