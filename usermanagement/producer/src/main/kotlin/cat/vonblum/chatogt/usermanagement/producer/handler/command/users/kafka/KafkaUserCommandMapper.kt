2e99b4139e0d1cbb346ae67e0d408f2f27dcc3c9
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-12-07 14:15:21 +0100

    wip

--- usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/handler/command/users/kafka/KafkaUserCommandMapper.kt
+++ usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/handler/command/users/kafka/KafkaUserCommandMapper.kt
@@ -1,11 +1,14 @@
 package cat.vonblum.chatogt.usermanagement.producer.handler.command.users.kafka
 
+import cat.vonblum.chatogt.usermanagement.users.create.CreateUserCommand
 import user.User
 
 class KafkaUserCommandMapper {
 
-    fun toDomain(request: User.CreateUserRequest) {
-        TODO("Not yet implemented")
-    }
+    fun toDomain(request: User.CreateUserRequest) =
+        CreateUserCommand(
+            request.email,
+            request.password,
+        )
 
 }
