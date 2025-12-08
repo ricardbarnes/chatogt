50972c4d3bf9ad22a58de8cc934e96862a386b9a
Ricard Barnes <ricard.barnes@smartassets.ai>
2025-12-08 01:13:17 +0100

    wip

--- usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/bus/event/kafka/KafkaEventMapper.kt
+++ usermanagement/producer/src/main/kotlin/cat/vonblum/chatogt/usermanagement/producer/bus/event/kafka/KafkaEventMapper.kt
@@ -1,11 +1,16 @@
 package cat.vonblum.chatogt.usermanagement.producer.bus.event.kafka
 
 import cat.vonblum.chatogt.usermanagement.users.UserCreatedEvent
+import user.User
 
 class KafkaEventMapper {
 
     fun toInfra(event: UserCreatedEvent): ByteArray {
-        TODO()
+        return User.UserCreatedEvent.newBuilder()
+            .setEmail(event.email)
+            .setPassword(event.password)
+            .build()
+            .toByteArray()
     }
 
 }
