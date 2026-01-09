216ebbf8082e77e92df8eb8b54c390667c121cd9
Ricard Barnes <ricard.barnes@smartassets.ai>
2026-01-09 23:55:24 +0100

    wip

--- usermanagement/shared/src/main/kotlin/cat/vonblum/chatogt/usermanagement/shared/event/mongo/MongoEventStore.kt
+++ usermanagement/shared/src/main/kotlin/cat/vonblum/chatogt/usermanagement/shared/event/mongo/MongoEventStore.kt
@@ -4,17 +4,27 @@ import cat.vonblum.chatogt.usermanagement.domain.event.Event
 import cat.vonblum.chatogt.usermanagement.domain.valueobject.Id
 import cat.vonblum.chatogt.usermanagement.shared.event.EventStore
 import cat.vonblum.chatogt.usermanagement.shared.event.shared.ProtoEventMapper
+import cat.vonblum.chatogt.usermanagement.users.UserCreatedEvent
 
 class MongoEventStore(
     private val protoMapper: ProtoEventMapper,
 ) : EventStore {
 
     override fun append(event: Event) {
-        TODO("Not yet implemented")
+        when (event) {
+            is UserCreatedEvent -> append(event)
+            else -> throw IllegalArgumentException("event type is not handled: $event")
+        }
     }
 
     override fun load(aggregateId: Id): List<Event> {
         TODO("Not yet implemented")
+    }
+
+    private fun append(event: UserCreatedEvent) {
+        val proto = protoMapper.toInfra(event)
+        val serialized = proto.toByteArray()
+        TODO()
     }
 
 }
