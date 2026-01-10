898ab98b55fbde62d3e95dd60d9cc9bdcd77e9d8
Ricard Barnes <ricard.barnes@smartassets.ai>
2026-01-10 23:50:23 +0100

    wip

--- usermanagement/shared/src/main/kotlin/cat/vonblum/chatogt/usermanagement/shared/event/mongo/MongoEventStore.kt
+++ usermanagement/shared/src/main/kotlin/cat/vonblum/chatogt/usermanagement/shared/event/mongo/MongoEventStore.kt
@@ -6,11 +6,13 @@ import cat.vonblum.chatogt.usermanagement.shared.event.EventStore
 import cat.vonblum.chatogt.usermanagement.shared.event.shared.ProtoEventDeserializer
 import cat.vonblum.chatogt.usermanagement.shared.event.shared.ProtoEventSerializer
 import org.springframework.data.mongodb.core.MongoTemplate
+import kotlin.reflect.KClass
 
 class MongoEventStore(
     private val template: MongoTemplate,
     private val serializer: ProtoEventSerializer,
-    private val deserializer: ProtoEventDeserializer
+    private val deserializer: ProtoEventDeserializer,
+    private val collectionMap: Map<KClass<out Event>, String>
 ) : EventStore {
 
     override fun append(
