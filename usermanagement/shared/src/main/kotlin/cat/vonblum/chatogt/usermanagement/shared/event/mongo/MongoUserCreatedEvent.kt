5f4882cea8f5458acec83d7b5d8e6eb6751465f8
Ricard Barnes <ricard.barnes@smartassets.ai>
2026-01-25 23:10:53 +0100

    wip

--- usermanagement/shared/src/main/kotlin/cat/vonblum/chatogt/usermanagement/shared/event/mongo/MongoUserCreatedEvent.kt
+++ usermanagement/shared/src/main/kotlin/cat/vonblum/chatogt/usermanagement/shared/event/mongo/MongoUserCreatedEvent.kt
@@ -1,11 +1,20 @@
 package cat.vonblum.chatogt.usermanagement.shared.event.mongo
 
 import cat.vonblum.chatogt.usermanagement.shared.event.mongo.MongoEvent.Companion.USERS_COLLECTION
+import org.springframework.data.mongodb.core.index.CompoundIndex
+import org.springframework.data.mongodb.core.index.CompoundIndexes
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 
 @Document(collection = USERS_COLLECTION)
+@CompoundIndexes(
+    CompoundIndex(
+        name = "aggregateId_aggregateVersion_idx",
+        def = "{'aggregateId': 1, 'aggregateVersion': 1}",
+        unique = true
+    )
+)
 class MongoUserCreatedEvent(
     id: String,
     aggregateId: String,
