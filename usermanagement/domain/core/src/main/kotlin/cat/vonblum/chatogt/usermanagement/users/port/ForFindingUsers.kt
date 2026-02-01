ff665ab4c0390adbc6e8721ff529dd579ad4b765
Ricard Barnes <ricard.barnes@smartassets.ai>
2026-02-02 00:50:51 +0100

    wip

--- usermanagement/domain/core/src/main/kotlin/cat/vonblum/chatogt/usermanagement/users/port/ForFindingUsers.kt
+++ usermanagement/domain/core/src/main/kotlin/cat/vonblum/chatogt/usermanagement/users/port/ForFindingUsers.kt
@@ -1,13 +1,13 @@
 package cat.vonblum.chatogt.usermanagement.users.port
 
+import cat.vonblum.chatogt.usermanagement.users.model.User
 import cat.vonblum.chatogt.usermanagement.users.model.UserEmail
 import cat.vonblum.chatogt.usermanagement.users.model.UserId
-import cat.vonblum.chatogt.usermanagement.users.view.UserView
 
 interface ForFindingUsers {
 
-    fun findById(id: UserId): UserView
+    fun findById(id: UserId): User
 
-    fun findByEmail(name: UserEmail): UserView
+    fun findByEmail(name: UserEmail): User
 
 }
